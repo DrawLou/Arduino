78cb99d27deb60f2d4e2d694cc4bf09eaece9649
Louise <zhang.louise@hotmail.fr>
2023-06-24 16:30:58 +0200

    setup ecran lcd et bluetooth

--- test2/test2.ino
+++ test2/test2.ino
@@ -1,9 +1,28 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <SoftwareSerial.h>
 
+#include "Wire.h"
+#include "LiquidCrystal_I2C.h"
+
+SoftwareSerial bluetoothSerial(10, 11); // RX, TX
+
+LiquidCrystal_I2C LCD(0x27,20,4); // définit le type d'écran lcd 16 x 2
+
+void setup() {
+   LCD.init(); // initialisation de l'afficheur
+   LCD.backlight();
+   
+   LCD.setCursor(0, 0);
+   LCD.print("Etat du bac :");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (bluetoothSerial.available()) {
+    String message = bluetoothSerial.readStringUntil('\n');
+    lcd.setCursor(0, 1);
+    lcd.print(message);
+  } else {
+    LCD.setCursor(0, 1);
+    LCD.print("depose possible");
+  }
 
 }
