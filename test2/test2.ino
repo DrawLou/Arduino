9fd13ae78fd6b33c69aa3c933ffc23ec9ea364b4
Louise <zhang.louise@hotmail.fr>
2023-06-25 20:55:05 +0200

    add button

--- test2/test2.ino
+++ test2/test2.ino
@@ -6,6 +6,9 @@ SoftwareSerial bluetoothSerial(2, 3); // RX, TX
 
 LiquidCrystal_I2C lcd (0x27,20,4); // définit le type d'écran lcd 16 x 2
 
+const int boutonPin = 2;
+bool boutonPresse = false;
+
 void setup() {
    lcd.init(); // initialisation de l'afficheur
    lcd.backlight();
@@ -26,6 +29,17 @@ void loop() {
   } else {
     lcd.setCursor(0, 1);
     lcd.print("depose possible");
+  }
+
+  // Vérifier l'état du bouton
+  if (digitalRead(boutonPin) == LOW) {
+    boutonPresse = true;
+  } else {
+    if (boutonPresse) {
+      // Envoyer un message spécial à l'émetteur pour reprendre le programme
+      bluetoothSerial.println("Reprendre programme");
+      boutonPresse = false;
+    }
   }
 
 }
